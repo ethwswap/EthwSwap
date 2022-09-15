f526b41af0d050f9f547ac172c4d1fe506f2d9b2
Ethwswap <ethwswap@gmail.com>
2022-09-15 15:47:46 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -188,20 +188,49 @@
         <div class="flex items-center space-x-4">
           <span class="hover:text-primary-500 text-white cursor-pointer text-fs16" @click="FAQModal = true">FAQ</span>
         </div>
-        <div class="flex items-center justify-between space-x-4 mt-2">
+        <div class="flex items-center justify-between space-x-2 mt-2">
           <div class="text-white opacity-60">Media inquires for Ethwswap Labs - Contact ethwswap@protonmail.com</div>
           <div class="flex items-center space-x-3">
             <NuxtLink hover="text-primary-500 opacity-100" class="text-fs24 transition delay-50 text-white opacity-60"
-              i-carbon-logo-twitter to="https://twitter.com/EthwSwap" target="_blank"></NuxtLink>
+              to="https://twitter.com/EthwSwap" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24">
+                <path fill="currentColor"
+                  d="M23.643 4.937c-.835.37-1.732.62-2.675.733a4.67 4.67 0 0 0 2.048-2.578a9.3 9.3 0 0 1-2.958 1.13a4.66 4.66 0 0 0-7.938 4.25a13.229 13.229 0 0 1-9.602-4.868c-.4.69-.63 1.49-.63 2.342A4.66 4.66 0 0 0 3.96 9.824a4.647 4.647 0 0 1-2.11-.583v.06a4.66 4.66 0 0 0 3.737 4.568a4.692 4.692 0 0 1-2.104.08a4.661 4.661 0 0 0 4.352 3.234a9.348 9.348 0 0 1-5.786 1.995a9.5 9.5 0 0 1-1.112-.065a13.175 13.175 0 0 0 7.14 2.093c8.57 0 13.255-7.098 13.255-13.254c0-.2-.005-.402-.014-.602a9.47 9.47 0 0 0 2.323-2.41l.002-.003Z" />
+              </svg>
+            </NuxtLink>
             <NuxtLink hover="text-primary-500 opacity-100" class="text-fs24 transition delay-50 text-white opacity-60"
-              i-carbon-logo-discord to="https://discord.gg/ZC4KykPamV" target="_blank"></NuxtLink>
+              to="https://discord.gg/ZC4KykPamV" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24">
+                <path fill="currentColor"
+                  d="M20.317 4.492c-1.53-.69-3.17-1.2-4.885-1.49a.075.075 0 0 0-.079.036c-.21.369-.444.85-.608 1.23a18.566 18.566 0 0 0-5.487 0a12.36 12.36 0 0 0-.617-1.23A.077.077 0 0 0 8.562 3c-1.714.29-3.354.8-4.885 1.491a.07.07 0 0 0-.032.027C.533 9.093-.32 13.555.099 17.961a.08.08 0 0 0 .031.055a20.03 20.03 0 0 0 5.993 2.98a.078.078 0 0 0 .084-.026a13.83 13.83 0 0 0 1.226-1.963a.074.074 0 0 0-.041-.104a13.201 13.201 0 0 1-1.872-.878a.075.075 0 0 1-.008-.125c.126-.093.252-.19.372-.287a.075.075 0 0 1 .078-.01c3.927 1.764 8.18 1.764 12.061 0a.075.075 0 0 1 .079.009c.12.098.245.195.372.288a.075.075 0 0 1-.006.125c-.598.344-1.22.635-1.873.877a.075.075 0 0 0-.041.105c.36.687.772 1.341 1.225 1.962a.077.077 0 0 0 .084.028a19.963 19.963 0 0 0 6.002-2.981a.076.076 0 0 0 .032-.054c.5-5.094-.838-9.52-3.549-13.442a.06.06 0 0 0-.031-.028ZM8.02 15.278c-1.182 0-2.157-1.069-2.157-2.38c0-1.312.956-2.38 2.157-2.38c1.21 0 2.176 1.077 2.157 2.38c0 1.312-.956 2.38-2.157 2.38Zm7.975 0c-1.183 0-2.157-1.069-2.157-2.38c0-1.312.955-2.38 2.157-2.38c1.21 0 2.176 1.077 2.157 2.38c0 1.312-.946 2.38-2.157 2.38Z" />
+              </svg>
+            </NuxtLink>
             <NuxtLink hover="text-primary-500 opacity-100" class="text-fs24 transition delay-50 text-white opacity-60"
               to="https://t.me/ethwswap_en" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet"
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" preserveAspectRatio="xMidYMid meet"
                 viewBox="0 0 24 24">
                 <path fill="currentColor" fill-rule="evenodd"
                   d="M24 12c0 6.627-5.373 12-12 12S0 18.627 0 12S5.373 0 12 0s12 5.373 12 12ZM12.43 8.859c-1.167.485-3.5 1.49-6.998 3.014c-.568.226-.866.447-.893.663c-.046.366.412.51 1.034.705c.085.027.173.054.263.084c.613.199 1.437.432 1.865.441c.389.008.823-.152 1.302-.48c3.268-2.207 4.955-3.322 5.061-3.346c.075-.017.179-.039.249.024c.07.062.063.18.056.212c-.046.193-1.84 1.862-2.77 2.726c-.29.269-.495.46-.537.504c-.094.097-.19.19-.282.279c-.57.548-.996.96.024 1.632c.49.323.882.59 1.273.856c.427.291.853.581 1.405.943c.14.092.274.187.405.28c.497.355.944.673 1.496.623c.32-.03.652-.331.82-1.23c.397-2.126 1.179-6.73 1.36-8.628a2.111 2.111 0 0 0-.02-.472a.506.506 0 0 0-.172-.325c-.143-.117-.365-.142-.465-.14c-.451.008-1.143.249-4.476 1.635Z"
                   clip-rule="evenodd" />
+              </svg>
+            </NuxtLink>
+            <NuxtLink hover="text-primary-500 opacity-100" class="text-fs24 transition delay-50 text-white opacity-60"
+              i-carbon-logo-github to="https://github.com/ethwswap/EthwSwap" target="_blank"></NuxtLink>
+            <NuxtLink hover="text-primary-500 opacity-100" class="text-fs24 transition delay-50 text-white opacity-60"
+              to="https://etherscan.io/address/0xd78195e2815932f2986214c462e6bf209f326b3a#code" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 32 32">
+                <g fill="currentColor" fill-rule="evenodd">
+                  <path
+                    d="M16 32C7.163 32 0 24.837 0 16S7.163 0 16 0s16 7.163 16 16s-7.163 16-16 16zm7.994-15.781L16.498 4L9 16.22l7.498 4.353l7.496-4.354zM24 17.616l-7.502 4.351L9 17.617l7.498 10.378L24 17.616z" />
+                  <g fill-rule="nonzero">
+                    <path fill-opacity=".298" d="M16.498 4v8.87l7.497 3.35zm0 17.968v6.027L24 17.616z" />
+                    <path fill-opacity=".801" d="m16.498 20.573l7.497-4.353l-7.497-3.348z" />
+                    <path fill-opacity=".298" d="m9 16.22l7.498 4.353v-7.701z" />
+                  </g>
+                </g>
               </svg>
             </NuxtLink>
           </div>
